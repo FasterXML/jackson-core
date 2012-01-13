f165bebee3f2f404696dbece5064b87876535a3b
Sebastian Thelen <sebastian.thelen@ima-zlw-ifu.rwth-aachen.de>
2012-01-13 22:33:12 +0100

    added two missing overrides to JsonParserDelegate
    implementing delegation of methods getEmbeddedObject() and
    getBooleanValue() in JsonParserDelegate

--- src/main/java/com/fasterxml/jackson/core/util/JsonParserDelegate.java
+++ src/main/java/com/fasterxml/jackson/core/util/JsonParserDelegate.java
@@ -218,6 +218,26 @@ public class JsonParserDelegate extends JsonParser
     public Number getNumberValue() throws IOException, JsonParseException {
         return delegate.getNumberValue();
     }
+    
+    /*
+    /**********************************************************
+    /* Public API, access to token information, other
+    /**********************************************************
+     */
+    
+    @Override
+    public boolean getBooleanValue()
+        throws IOException, JsonParseException
+    {
+        return delegate.getBooleanValue();
+    }
+
+    @Override
+    public Object getEmbeddedObject()
+        throws IOException, JsonParseException
+    {
+        return delegate.getEmbeddedObject();
+    }
 
     @Override
     public byte[] getBinaryValue(Base64Variant b64variant) throws IOException, JsonParseException {

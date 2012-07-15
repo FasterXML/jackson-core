9e41adf85f28f95775ce3d11cbfccf9f10e83f5f
Tatu Saloranta <tsaloranta@gmail.com>
2012-07-15 10:57:23 -0700

    Fix [JACKSON-853] for 2.0.5

--- src/test/java/com/fasterxml/jackson/core/io/TestJsonStringEncoder.java
+++ src/test/java/com/fasterxml/jackson/core/io/TestJsonStringEncoder.java
@@ -20,6 +20,24 @@ public class TestJsonStringEncoder
         assertArrayEquals("\\\"x\\\"".toCharArray(), result);
     }
 
+    // For [JACKSON-853]
+    public void testQuoteLongAsString() throws Exception
+    {
+        JsonStringEncoder encoder = new JsonStringEncoder();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < 1111; ++i) {
+            sb.append('"');
+            sb2.append("\\\"");
+        }
+        String input = sb.toString();
+        String exp = sb2.toString();
+        char[] result = encoder.quoteAsString(input);
+        assertEquals(2*input.length(), result.length);
+        assertEquals(exp, new String(result));
+        
+    }
+    
     public void testQuoteAsUTF8() throws Exception
     {
         // In this case, let's actually use existing JsonGenerator to produce expected values

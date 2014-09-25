ea6d687b610debdaf312f7db137080eb39145802
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-09-24 20:18:47 -0700

    Backport fix #152 to 2.3 branch

--- src/test/java/com/fasterxml/jackson/core/util/TestTextBuffer.java
+++ src/test/java/com/fasterxml/jackson/core/util/TestTextBuffer.java
@@ -62,4 +62,19 @@ public class TestTextBuffer
          assertEquals(len+2, tb.size());
          assertEquals(EXP, tb.contentsAsString());
       }
+
+      // [Core#152]
+      public void testExpand()
+      {
+          TextBuffer tb = new TextBuffer(new BufferRecycler());
+          char[] buf = tb.getCurrentSegment();
+
+          while (buf.length < 500 * 1000) {
+              char[] old = buf;
+              buf = tb.expandCurrentSegment();
+              if (old.length >= buf.length) {
+                  fail("Expected buffer of "+old.length+" to expand, did not, length now "+buf.length);
+              }
+          }
+      }
 }

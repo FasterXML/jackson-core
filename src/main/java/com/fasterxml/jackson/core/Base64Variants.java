3dcedd2b6838ef29abb179557b6e42479e93834b
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-01-10 20:15:06 -0800

    Fix #178. Also fix a few javadoc processor warnings

--- src/main/java/com/fasterxml/jackson/core/Base64Variants.java
+++ src/main/java/com/fasterxml/jackson/core/Base64Variants.java
@@ -23,7 +23,7 @@ public final class Base64Variants
      * This variant is what most people would think of "the standard"
      * Base64 encoding.
      *<p>
-     * See <a href="">wikipedia Base64 entry</a> for details.
+     * See <a href="http://en.wikipedia.org/wiki/Base64">wikipedia Base64 entry</a> for details.
      *<p>
      * Note that although this can be thought of as the standard variant,
      * it is <b>not</b> the default for Jackson: no-linefeeds alternative
--- src/main/java/com/fasterxml/jackson/core/util/DefaultPrettyPrinter.java
+++ src/main/java/com/fasterxml/jackson/core/util/DefaultPrettyPrinter.java
@@ -412,21 +412,36 @@ public class DefaultPrettyPrinter
     @Deprecated
     public static class Lf2SpacesIndenter extends DefaultIndenter
     {
-        /** @deprecated Use {@link DefaultIndenter#SYSTEM_LINEFEED_INSTANCE} instead */
+        /** @deprecated Use {@link DefaultIndenter#SYSTEM_LINEFEED_INSTANCE} instead.
+         */
         @SuppressWarnings("hiding")
         @Deprecated
         public static final Lf2SpacesIndenter instance = new Lf2SpacesIndenter();
 
-        /** @deprecated Use {@code new DefaultIndenter("  ", DefaultIndenter.SYS_LF)} instead */
+        /** @deprecated Use {@code new DefaultIndenter("  ", DefaultIndenter.SYS_LF)} instead
+         */
         @Deprecated
         public Lf2SpacesIndenter() {
             super("  ", DefaultIndenter.SYS_LF);
         }
         
-        /** @deprecated Use {@code new DefaultIndenter("  ", lf)} instead */
+        /** @deprecated Use {@code new DefaultIndenter("  ", lf)} instead
+         */
         @Deprecated
         public Lf2SpacesIndenter(String lf) {
             super("  ", lf);
+        }
+
+        /**
+         * Note: method was accidentally missing from 2.5.0; put back for 2.5.1 and
+         * later 2.5.x versions.
+         */
+        @Override
+        public Lf2SpacesIndenter withLinefeed(String lf) {
+            if (lf.equals(getEol())) {
+                return this;
+            }
+            return new Lf2SpacesIndenter(lf);
         }
     }
 }

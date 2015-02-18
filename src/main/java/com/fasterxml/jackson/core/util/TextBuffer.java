11f0b4090937b2aa998734aa2bf032ee8c428e84
Masaru Hasegawa <masaru@citra.local>
2015-02-18 11:47:46 +0900

    Always return empty array instead of null for empty buffer
    Fixes #182

--- src/main/java/com/fasterxml/jackson/core/util/TextBuffer.java
+++ src/main/java/com/fasterxml/jackson/core/util/TextBuffer.java
@@ -304,7 +304,7 @@ public final class TextBuffer
             return (_resultArray = _resultString.toCharArray());
         }
         // Nope; but does it fit in just one segment?
-        if (!_hasSegments)  return _currentSegment;
+        if (!_hasSegments && _currentSegment != null)  return _currentSegment;
         // Nope, need to have/create a non-segmented array and return it
         return contentsAsArray();
     }

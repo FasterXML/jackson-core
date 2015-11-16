2bb14b83d9ff7ff392973a0fa0ce860100cfc305
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-11-15 22:46:48 -0800

    Add failing tests for #37, #229

--- src/test/java/com/fasterxml/jackson/core/json/TestLocation.java
+++ src/test/java/com/fasterxml/jackson/core/json/TestLocation.java
@@ -5,16 +5,17 @@ import com.fasterxml.jackson.core.*;
 // NOTE: just a stub so for, fill me!
 public class TestLocation extends com.fasterxml.jackson.core.BaseTest
 {
+    final JsonFactory JSON_F = new JsonFactory();
+
     // Trivially simple unit test for basics wrt offsets
     public void testSimpleInitialOffsets() throws Exception
     {
-        final JsonFactory f = new JsonFactory();
         JsonLocation loc;
         JsonParser p;
         final String DOC = "{ }";
 
         // first, char based:
-        p = f.createParser(DOC);
+        p = JSON_F.createParser(DOC);
         assertToken(JsonToken.START_OBJECT, p.nextToken());
 
         loc = p.getTokenLocation();
@@ -33,7 +34,7 @@ public class TestLocation extends com.fasterxml.jackson.core.BaseTest
 
         // then byte-based
         
-        p = f.createParser(DOC.getBytes("UTF-8"));
+        p = JSON_F.createParser(DOC.getBytes("UTF-8"));
         assertToken(JsonToken.START_OBJECT, p.nextToken());
 
         loc = p.getTokenLocation();
@@ -51,17 +52,16 @@ public class TestLocation extends com.fasterxml.jackson.core.BaseTest
         p.close();
     }
 
-    // for [Issue#111]
+    // for [core#111]
     public void testOffsetWithInputOffset() throws Exception
     {
-        final JsonFactory f = new JsonFactory();
         JsonLocation loc;
         JsonParser p;
         // 3 spaces before, 2 after, just for padding
         byte[] b = "   { }  ".getBytes("UTF-8");
 
         // and then peel them off
-        p = f.createParser(b, 3, b.length-5);
+        p = JSON_F.createParser(b, 3, b.length-5);
         assertToken(JsonToken.START_OBJECT, p.nextToken());
 
         loc = p.getTokenLocation();
---
+++ src/test/java/com/fasterxml/jackson/failing/LocationInArrayTest.java
@@ -0,0 +1,57 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.core.*;
+
+// Tests mostly for [core#229]
+public class LocationInArrayTest extends com.fasterxml.jackson.core.BaseTest
+{
+    final JsonFactory JSON_F = new JsonFactory();
+
+    // for [core#229]
+    public void testOffsetInArraysBytes() throws Exception {
+        _testOffsetInArrays(true);
+    }
+    
+    // for [core#229]
+    public void testOffsetInArraysChars() throws Exception {
+        _testOffsetInArrays(false);
+    }        
+
+    private void _testOffsetInArrays(boolean useBytes) throws Exception
+    {
+        JsonParser p;
+        final String DOC = "  [10, 251,\n   3  ]";
+
+        // first, char based:
+        p = useBytes ? JSON_F.createParser(DOC.getBytes("UTF-8"))
+                : JSON_F.createParser(DOC.toCharArray());
+        assertToken(JsonToken.START_ARRAY, p.nextToken());
+        _assertLocation(useBytes, p.getTokenLocation(), 2L, 1, 3);
+        _assertLocation(useBytes, p.getCurrentLocation(), 3L, 1, 4);
+
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        _assertLocation(useBytes, p.getTokenLocation(), 3L, 1, 4);
+        assertEquals(10, p.getIntValue()); // just to ensure read proceeds to end
+        // 2-digits so
+        _assertLocation(useBytes, p.getCurrentLocation(), 5L, 1, 6);
+
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        _assertLocation(useBytes, p.getTokenLocation(), 7L, 1, 8);
+        assertEquals(251, p.getIntValue()); // just to ensure read proceeds to end
+        _assertLocation(useBytes, p.getCurrentLocation(), 10L, 1, 11);
+        
+        p.close();
+    }
+
+    private void _assertLocation(boolean useBytes, JsonLocation loc, long offset, int row, int col)
+    {
+        assertEquals(row, loc.getLineNr());
+        assertEquals(col, loc.getColumnNr());
+
+        if (useBytes) {
+            assertEquals(offset, loc.getByteOffset());
+        } else {
+            assertEquals(offset, loc.getCharOffset());
+        }
+    }
+}

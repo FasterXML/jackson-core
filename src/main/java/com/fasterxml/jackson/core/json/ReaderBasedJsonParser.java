52f219f234bd17120cff30f93d0cb2b2a8118d01
Ferooz Khan <ferooz@codenation.co.in>
2016-09-26 12:35:01 +0400

    Refactored Long Method : com.fasterxml.jackson.core.json.ReaderBasedJsonParser.java

--- src/main/java/com/fasterxml/jackson/core/json/ReaderBasedJsonParser.java
+++ src/main/java/com/fasterxml/jackson/core/json/ReaderBasedJsonParser.java
@@ -929,51 +929,54 @@ public class ReaderBasedJsonParser // final in 2.3, earlier
             _nextToken = JsonToken.VALUE_STRING;
             return name;
         }
+
+        JsonToken token = getToken(i);
         
-        // Ok: we must have a value... what is it?
-
-        JsonToken t;
-
-        switch (i) {
-        case '-':
-            t = _parseNegNumber();
-            break;
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            t = _parsePosNumber(i);
-            break;
-        case 'f':
-            _matchFalse();
-            t = JsonToken.VALUE_FALSE;
-            break;
-        case 'n':
-            _matchNull();
-            t = JsonToken.VALUE_NULL;
-            break;
-        case 't':
-            _matchTrue();
-            t = JsonToken.VALUE_TRUE;
-            break;
-        case '[':
-            t = JsonToken.START_ARRAY;
-            break;
-        case '{':
-            t = JsonToken.START_OBJECT;
-            break;
-        default:
-            t = _handleOddValue(i);
-            break;
-        }
-        _nextToken = t;
+        _nextToken = token;
         return name;
+    }
+
+    private JsonToken getToken(int i) throws java.io.IOException {
+        JsonToken t;
+        switch (i) {
+            case '-':
+                t = _parseNegNumber();
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                t = _parsePosNumber(i);
+                break;
+            case 'f':
+                _matchFalse();
+                t = JsonToken.VALUE_FALSE;
+                break;
+            case 'n':
+                _matchNull();
+                t = JsonToken.VALUE_NULL;
+                break;
+            case 't':
+                _matchTrue();
+                t = JsonToken.VALUE_TRUE;
+                break;
+            case '[':
+                t = JsonToken.START_ARRAY;
+                break;
+            case '{':
+                t = JsonToken.START_OBJECT;
+                break;
+            default:
+                t = _handleOddValue(i);
+                break;
+        }
+        return t;
     }
 
     private final void _isNextTokenNameYes(int i) throws IOException

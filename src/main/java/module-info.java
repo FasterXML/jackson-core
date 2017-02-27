c21da17d1eb2c2c0ca9139e6180515add7d008fe
Lovro Pandzic <lovro.pandzic@gmail.com>
2017-02-27 21:49:29 +0100

    modularised jackson-core

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module jackson_core {
+    exports com.fasterxml.jackson.core;
+    exports com.fasterxml.jackson.core.base;
+    exports com.fasterxml.jackson.core.filter;
+    exports com.fasterxml.jackson.core.format;
+    exports com.fasterxml.jackson.core.io;
+    exports com.fasterxml.jackson.core.json;
+    exports com.fasterxml.jackson.core.sym;
+    exports com.fasterxml.jackson.core.type;
+    exports com.fasterxml.jackson.core.util;
+}

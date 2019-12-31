ac3f7d4bf4e62f27b9d01b3081fff109d68f0efa
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-12-30 21:59:38 -0800

    test coverage

--- src/test/java/com/fasterxml/jackson/core/json/JsonFactoryTest.java
+++ src/test/java/com/fasterxml/jackson/core/json/JsonFactoryTest.java
@@ -96,19 +96,28 @@ public class JsonFactoryTest
         }
     }
 
+    static class BogusSchema implements FormatSchema
+    {
+        @Override
+        public String getSchemaType() {
+            return "test";
+        }
+    }
+
     /*
     /**********************************************************************
     /* Test methods
     /**********************************************************************
      */
+
+    final JsonFactory JSON_F = sharedStreamFactory();
     
     @SuppressWarnings("deprecation")
     public void testGeneratorFeatures() throws Exception
     {
-        JsonFactory f = new JsonFactory();
-        assertNull(f.getCodec());
+        assertNull(JSON_F.getCodec());
 
-        f = JsonFactory.builder()
+        JsonFactory f = JsonFactory.builder()
                 .configure(JsonWriteFeature.QUOTE_FIELD_NAMES, true)
                 .build();
         // 24-Oct-2018, tatu: Until 3.x, we'll only have backwards compatible
@@ -128,6 +137,24 @@ public class JsonFactoryTest
 
         // by default, should be enabled
         assertTrue(f.isEnabled(JsonFactory.Feature.USE_THREAD_LOCAL_FOR_BUFFER_RECYCLING));
+
+        assertFalse(JSON_F.requiresCustomCodec());
+        assertFalse(JSON_F.canHandleBinaryNatively());
+    }
+
+    public void testFactoryMisc() throws Exception
+    {
+        assertNull(JSON_F.getInputDecorator());
+        assertNull(JSON_F.getOutputDecorator());
+
+        assertFalse(JSON_F.canUseSchema(null));
+        assertFalse(JSON_F.canUseSchema(new BogusSchema()));
+
+        assertNull(JSON_F.getFormatReadFeatureType());
+        assertNull(JSON_F.getFormatWriteFeatureType());
+
+        assertEquals(0, JSON_F.getFormatParserFeatures());
+        assertEquals(0, JSON_F.getFormatGeneratorFeatures());
     }
 
     // for [core#189]: verify that it's ok to disable recycling

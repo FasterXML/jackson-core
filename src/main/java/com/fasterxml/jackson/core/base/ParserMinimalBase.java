ee116a041354ee2d4dccb13ee0deb18cd9880820
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-05-20 22:44:00 -0700

    ... add import that was missing

--- src/main/java/com/fasterxml/jackson/core/base/ParserMinimalBase.java
+++ src/main/java/com/fasterxml/jackson/core/base/ParserMinimalBase.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.core.sym.FieldNameMatcher;
 import com.fasterxml.jackson.core.type.ResolvedType;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.core.util.ByteArrayBuilder;
+import com.fasterxml.jackson.core.util.JacksonFeatureSet;
 import com.fasterxml.jackson.core.util.VersionUtil;
 
 import static com.fasterxml.jackson.core.JsonTokenId.*;

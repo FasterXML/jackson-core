f3a05ad06e8ef57d1034c65a52ef4dcbd647abab
Pavel Krutikhin <krutikhin92@gmail.com>
2020-10-25 23:50:29 +0700

    Fixed NPE in writeNumber(String) method of UTF8JsonGenerator and WriterBasedJsonGenerator

--- src/main/java/com/fasterxml/jackson/core/json/WriterBasedJsonGenerator.java
+++ src/main/java/com/fasterxml/jackson/core/json/WriterBasedJsonGenerator.java
@@ -829,7 +829,9 @@ public class WriterBasedJsonGenerator
     public void writeNumber(String encodedValue) throws IOException
     {
         _verifyValueWrite(WRITE_NUMBER);
-        if (_cfgNumbersAsStrings) {
+        if (encodedValue == null) {
+            _writeNull();
+        } else if (_cfgNumbersAsStrings) {
             _writeQuotedRaw(encodedValue);
         } else {
             writeRaw(encodedValue);

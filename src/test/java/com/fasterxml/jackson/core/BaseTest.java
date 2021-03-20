45126bf09d0fcba97b9f0f0033f363dc7bf84d7e
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-03-20 15:02:26 -0700

    minor addition to BaseTest

--- src/test/java/com/fasterxml/jackson/core/BaseTest.java
+++ src/test/java/com/fasterxml/jackson/core/BaseTest.java
@@ -624,4 +624,27 @@ public abstract class BaseTest
         }
         return result;
     }
+
+    protected byte[] readResource(String ref)
+    {
+       ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+       final byte[] buf = new byte[4000];
+
+       InputStream in = getClass().getResourceAsStream(ref);
+       if (in != null) {
+           try {
+               int len;
+               while ((len = in.read(buf)) > 0) {
+                   bytes.write(buf, 0, len);
+               }
+               in.close();
+           } catch (IOException e) {
+               throw new RuntimeException("Failed to read resource '"+ref+"': "+e);
+           }
+       }
+       if (bytes.size() == 0) {
+           throw new IllegalArgumentException("Failed to read resource '"+ref+"': empty resource?");
+       }
+       return bytes.toByteArray();
+    }
 }

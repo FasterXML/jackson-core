c518d663bdb2ca0d1e9376be990e9f81a53ba9e0
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-19 13:59:08 +0200

    remove workaround for old issue with a particular double

---
+++ src/test/java/com/fasterxml/jackson/core/io/TestNumberInput.java
@@ -0,0 +1,14 @@
+package com.fasterxml.jackson.core.io;
+
+public class TestNumberInput
+    extends com.fasterxml.jackson.core.BaseTest
+{
+    public void testNastySmallDouble()
+    {
+        //relates to https://github.com/FasterXML/jackson-core/issues/750
+        //prior to jackson v2.14, this value used to be returned as Double.MIN_VALUE
+        final String nastySmallDouble = "2.2250738585072012e-308";
+        assertEquals(Double.parseDouble(nastySmallDouble), NumberInput.parseDouble(nastySmallDouble));
+    }
+}
+

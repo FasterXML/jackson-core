9918e3e6c0f268c9a279005e3c2d72effbd5c600
PJ Fanning <pjfanning@users.noreply.github.com>
2022-06-21 01:33:09 +0100

    move feature

--- src/test/java/com/fasterxml/jackson/core/write/FastDoubleArrayGenerationTest.java
+++ src/test/java/com/fasterxml/jackson/core/write/FastDoubleArrayGenerationTest.java
@@ -1,10 +1,10 @@
 package com.fasterxml.jackson.core.write;
 
 import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.core.StreamWriteFeature;
 
 public class FastDoubleArrayGenerationTest extends ArrayGenerationTest {
-    private final JsonFactory FACTORY = JsonFactory.builder().enable(JsonWriteFeature.USE_FAST_DOUBLE_WRITER).build();
+    private final JsonFactory FACTORY = JsonFactory.builder().enable(StreamWriteFeature.USE_FAST_DOUBLE_WRITER).build();
 
     protected JsonFactory jsonFactory() {
         return FACTORY;

718f542784aec613d699fb82abdf5f25bbfd724f
PJ Fanning <pjfanning@users.noreply.github.com>
2022-06-22 00:55:57 +0100

    Support option to enable non-standard trailing decimal point (#773)

--- src/test/java/com/fasterxml/jackson/core/read/NonStandardNumberParsingTest.java
+++ src/test/java/com/fasterxml/jackson/core/read/NonStandardNumberParsingTest.java
@@ -8,6 +8,7 @@ public class NonStandardNumberParsingTest
 {
     private final JsonFactory JSON_F = JsonFactory.builder()
             .enable(JsonReadFeature.ALLOW_LEADING_DECIMAL_POINT_FOR_NUMBERS)
+            .enable(JsonReadFeature.ALLOW_TRAILING_DECIMAL_POINT_FOR_NUMBERS)
             .build();
 
     protected JsonFactory jsonFactory() {
@@ -30,6 +31,22 @@ public class NonStandardNumberParsingTest
         }
     }
 
+    /**
+     * The format "NNN." (as opposed to "NNN") is not valid JSON, so this should fail
+     */
+    public void testTrailingDotInDecimal() throws Exception {
+        for (int mode : ALL_MODES) {
+            JsonParser p = createParser(mode, " 123. ");
+            try {
+                p.nextToken();
+                fail("Should not pass");
+            } catch (JsonParseException e) {
+                verifyException(e, "Decimal point not followed by a digit");
+            }
+            p.close();
+        }
+    }
+
     public void testLeadingDotInDecimalAllowedAsync() throws Exception {
         _testLeadingDotInDecimalAllowed(jsonFactory(), MODE_DATA_INPUT);
     }
@@ -43,6 +60,19 @@ public class NonStandardNumberParsingTest
         _testLeadingDotInDecimalAllowed(jsonFactory(), MODE_READER);
     }
 
+    public void testTrailingDotInDecimalAllowedAsync() throws Exception {
+        _testTrailingDotInDecimalAllowed(jsonFactory(), MODE_DATA_INPUT);
+    }
+
+    public void testTrailingDotInDecimalAllowedBytes() throws Exception {
+        _testTrailingDotInDecimalAllowed(jsonFactory(), MODE_INPUT_STREAM);
+        _testTrailingDotInDecimalAllowed(jsonFactory(), MODE_INPUT_STREAM_THROTTLED);
+    }
+
+    public void testTrailingDotInDecimalAllowedReader() throws Exception {
+        _testTrailingDotInDecimalAllowed(jsonFactory(), MODE_READER);
+    }
+
     private void _testLeadingDotInDecimalAllowed(JsonFactory f, int mode) throws Exception
     {
         JsonParser p = createParser(f, mode, " .125 ");
@@ -50,6 +80,16 @@ public class NonStandardNumberParsingTest
         assertEquals(0.125, p.getValueAsDouble());
         assertEquals("0.125", p.getDecimalValue().toString());
         assertEquals(".125", p.getText());
+        p.close();
+    }
+
+    private void _testTrailingDotInDecimalAllowed(JsonFactory f, int mode) throws Exception
+    {
+        JsonParser p = createParser(f, mode, " 125. ");
+        assertEquals(JsonToken.VALUE_NUMBER_FLOAT, p.nextToken());
+        assertEquals(125.0, p.getValueAsDouble());
+        assertEquals("125", p.getDecimalValue().toString());
+        assertEquals("125.", p.getText());
         p.close();
     }
 }

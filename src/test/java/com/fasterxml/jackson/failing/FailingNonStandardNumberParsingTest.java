44285e98af8cea6a0ff41a092c0943efaab5bcc2
PJ Fanning <pjfanning@users.noreply.github.com>
2022-06-25 00:02:01 +0100

    Update FailingNonStandardNumberParsingTest.java

--- src/test/java/com/fasterxml/jackson/failing/FailingNonStandardNumberParsingTest.java
+++ src/test/java/com/fasterxml/jackson/failing/FailingNonStandardNumberParsingTest.java
@@ -10,9 +10,7 @@ public class FailingNonStandardNumberParsingTest
     extends BaseTest
 {
     private final JsonFactory JSON_F = JsonFactory.builder()
-            .enable(JsonReadFeature.ALLOW_LEADING_PLUS_SIGN_FOR_NUMBERS)
             .enable(JsonReadFeature.ALLOW_LEADING_DECIMAL_POINT_FOR_NUMBERS)
-            .enable(JsonReadFeature.ALLOW_TRAILING_DECIMAL_POINT_FOR_NUMBERS)
             .build();
 
     protected JsonFactory jsonFactory() {
@@ -34,11 +32,14 @@ public class FailingNonStandardNumberParsingTest
 
     private void _testLeadingDotInNegativeDecimalAllowed(JsonFactory f, int mode) throws Exception
     {
-        try (JsonParser p = createParser(f, mode, " -.125 ")) {
+        JsonParser p = createParser(f, mode, " -.125 ");
+        try {
             assertEquals(JsonToken.VALUE_NUMBER_FLOAT, p.nextToken());
             assertEquals(-0.125, p.getValueAsDouble());
             assertEquals("-0.125", p.getDecimalValue().toString());
             assertEquals("-.125", p.getText());
+        } finally {
+            p.close();
         }
     }
 }

767a90561269719487b49491a03a79ee70334467
PJ Fanning <pjfanning@users.noreply.github.com>
2023-01-19 21:12:20 +0000

    Update TestLazyNumbers.java

--- src/test/java/com/fasterxml/jackson/core/io/TestLazyNumbers.java
+++ src/test/java/com/fasterxml/jackson/core/io/TestLazyNumbers.java
@@ -12,6 +12,11 @@ public class TestLazyNumbers
         LazyDouble ln = new LazyDouble(num, true);
         assertEquals(num, ln.getText());
         assertEquals(1.234e-2, ln.getNumber());
+
+        Double d = new Double(num);
+        LazyDouble ln2 = new LazyDouble(d);
+        assertEquals(d.toString(), ln2.getText());
+        assertEquals(d, ln2.getNumber());
     }
 
     public void testLazyFloat()
@@ -20,6 +25,11 @@ public class TestLazyNumbers
         LazyFloat lf = new LazyFloat(num, true);
         assertEquals(num, lf.getText());
         assertEquals(1.234e-2f, lf.getNumber());
+
+        Float f = new Float(num);
+        LazyFloat lf2 = new LazyFloat(f);
+        assertEquals(f.toString(), lf2.getText());
+        assertEquals(f, lf2.getNumber());
     }
 
     public void testLazyBigDecimal()
@@ -28,13 +38,22 @@ public class TestLazyNumbers
         LazyBigDecimal ld = new LazyBigDecimal(num, true);
         assertEquals(num, ld.getText());
         assertEquals(new BigDecimal(num), ld.getNumber());
+
+        BigDecimal bd = new BigDecimal(num);
+        LazyBigDecimal ld2 = new LazyBigDecimal(bd);
+        assertEquals(bd.toString(), ld2.getText());
+        assertEquals(bd, ld.getNumber());
     }
 
     public void testLazyBigInteger()
     {
         String num = "1234567890";
-        LazyBigInteger ld = new LazyBigInteger(num, true);
-        assertEquals(num, ld.getText());
-        assertEquals(new BigInteger(num), ld.getNumber());
+        LazyBigInteger ln = new LazyBigInteger(num, true);
+        assertEquals(num, ln.getText());
+        assertEquals(new BigInteger(num), ln.getNumber());
+
+        LazyBigInteger ln2 = new LazyBigInteger(new BigInteger(num));
+        assertEquals(num, ln2.getText());
+        assertEquals(new BigInteger(num), ln.getNumber());
     }
 }

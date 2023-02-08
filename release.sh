d08eddb4119a2ee3714339852c548d0a5255e326
Pedro Kaj Kjellerup Nacht <pnacht@google.com>
2023-02-08 19:45:33 +0000

    Fix server-id, validate version

--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# This script simulates the Maven Release Plugin, but only performs release:clean and
-# release:prepare. The release:perform step is handled by the CI when the tag is
-# pushed.
+# This script simulates the Maven Release Plugin, but only performs
+# release:clean and release:prepare. The release:perform step is handled by the
+# CI when the tag is pushed.
 #
 # However, release:perform on Git requires the release.properties file. We must
-# therefore modify the first commit created by release:prepare to include this file, and
-# then delete the file in the second commit.
+# therefore modify the first commit created by release:prepare to include this
+# file, and then delete the file in the second commit.
 #
-# This will ensure that release.properties is available to release:perform in the CI,
-# while keeping with the expectation that this file does not get commited (long-term) to
-# the repository.
+# This will ensure that release.properties is available to release:perform in
+# the CI, while keeping with the expectation that this file does not get
+# commited (long-term) to the repository.
 
 set -euo pipefail
 
@@ -21,7 +21,7 @@ set -euo pipefail
 git reset HEAD~1
 
 # delete tag created by release:prepare
-tag_name=$(git tag --points-at)
+tag_name="$(git tag --points-at)"
 git tag -d "$tag_name"
 
 # Add release.properties to that commit
@@ -31,7 +31,8 @@ git commit --amend --no-edit
 # recreate tag
 git tag "$tag_name" -m "[maven-release-plugin] copy for tag $tag_name"
 
-# Recreate second commit, removing release.properties from the repository
+# Recreate second commit (from release to SNAPSHOT), removing
+# release.properties from the repository
 git rm release.properties
 git add pom.xml
 git commit -m "[maven-release-plugin] prepare for next development iteration"
@@ -40,4 +41,5 @@ git commit -m "[maven-release-plugin] prepare for next development iteration"
 git push
 git push origin "$tag_name"
 
+# clean up
 rm pom.xml.releaseBackup

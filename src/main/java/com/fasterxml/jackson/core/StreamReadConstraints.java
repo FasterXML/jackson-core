baf5ca466ab3bf3aab0d734e370541453435259a
PJ Fanning <pjfanning@users.noreply.github.com>
2023-03-04 10:38:35 +0100

    fix javadoc after recent StreamConstraintsException changes

--- src/main/java/com/fasterxml/jackson/core/StreamReadConstraints.java
+++ src/main/java/com/fasterxml/jackson/core/StreamReadConstraints.java
@@ -160,7 +160,7 @@ public class StreamReadConstraints
      * Convenience method that can be used to verify that a floating-point
      * number of specified length does not exceed maximum specific by this
      * constraints object: if it does, a
-     * {@link NumberFormatException}
+     * {@link StreamConstraintsException}
      * is thrown.
      *
      * @param length Length of number in input units
@@ -179,7 +179,7 @@ public class StreamReadConstraints
      * Convenience method that can be used to verify that an integer
      * number of specified length does not exceed maximum specific by this
      * constraints object: if it does, a
-     * {@link NumberFormatException}
+     * {@link StreamConstraintsException}
      * is thrown.
      *
      * @param length Length of number in input units
@@ -198,7 +198,7 @@ public class StreamReadConstraints
      * Convenience method that can be used to verify that a String
      * of specified length does not exceed maximum specific by this
      * constraints object: if it does, a
-     * {@link JsonParseException}
+     * {@link StreamConstraintsException}
      * is thrown.
      *
      * @param length Length of string in input units

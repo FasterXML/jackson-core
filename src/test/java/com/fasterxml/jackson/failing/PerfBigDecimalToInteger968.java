350dfd261cf561cfc9f128b89ec0b9041cfccc5a
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-04-01 13:56:32 -0700

    Add new failing tests to help investigation

---
+++ src/test/java/com/fasterxml/jackson/failing/PerfBigDecimalToInteger968.java
@@ -0,0 +1,30 @@
+package com.fasterxml.jackson.failing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.*;
+
+// For [core#968]]
+public class PerfBigDecimalToInteger968
+{
+    private final JsonFactory JSON_F = new JsonFactory();
+    
+    // For [core#968]]: shouldn't take multiple seconds
+    @Test(timeout = 3000)
+    public void bigIntegerViaBigDecimal() throws Exception {
+        final String DOC = "1e20000000";
+
+        try (JsonParser p = JSON_F.createParser(DOC)) {
+            assertToken(JsonToken.VALUE_NUMBER_FLOAT, p.nextToken());
+            Assert.assertNotNull(p.getBigIntegerValue());
+        }
+    }
+
+    protected void assertToken(JsonToken expToken, JsonToken actToken)
+    {
+        if (actToken != expToken) {
+            Assert.fail("Expected token "+expToken+", current token "+actToken);
+        }
+    }
+}

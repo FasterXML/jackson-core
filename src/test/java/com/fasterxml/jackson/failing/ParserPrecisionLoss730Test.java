1e68d1c7604a3c974884c6a44115750dbd0f30c7
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-04-06 13:12:39 -0700

    Test fix: change ref to #730 (issue) from #733 (PR)

--- src/test/java/com/fasterxml/jackson/failing/ParserPrecisionLoss730Test.java
+++ src/test/java/com/fasterxml/jackson/failing/ParserPrecisionLoss730Test.java
@@ -7,40 +7,42 @@ import com.fasterxml.jackson.core.JsonParser;
 
 import java.io.StringWriter;
 
-public class ParserPrecisionLoss733Test extends BaseTest {
-    final JsonFactory JSON_F = newStreamFactory();
+// [jackson-core#730]
+public class ParserPrecisionLoss730Test extends BaseTest
+{
+    private final JsonFactory JSON_F = newStreamFactory();
 
+    // [jackson-core#730]
     /**
      * Attempt to pass a BigDecimal value through without losing precision,
      * e.g. for pretty printing a file.
      */
     public void testCopyCurrentEventBigDecimal() throws Exception {
         String input = "1e999";
-        JsonParser parser = JSON_F.createParser(input);
-        parser.nextToken();
         StringWriter stringWriter = new StringWriter();
-        JsonGenerator generator = JSON_F.createGenerator(stringWriter);
-        generator.copyCurrentEvent(parser);
-        parser.close();
-        generator.close();
-        String actual = stringWriter.toString();
-        assertEquals(input, actual);
+
+        try (JsonParser parser = JSON_F.createParser(input)) {
+            parser.nextToken();
+            try (JsonGenerator generator = JSON_F.createGenerator(stringWriter)) {
+                generator.copyCurrentEvent(parser);
+            }
+        }
+        assertEquals(input, stringWriter.toString());
     }
 
+    // [jackson-core#730]
     /**
      * Same as {@link #testCopyCurrentEventBigDecimal()} using copyCurrentStructure instead.
      */
     public void testCopyCurrentStructureBigDecimal() throws Exception {
         String input = "[1e999]";
-        JsonParser parser = JSON_F.createParser(input);
-        parser.nextToken();
         StringWriter stringWriter = new StringWriter();
-        JsonGenerator generator = JSON_F.createGenerator(stringWriter);
-        generator.copyCurrentStructure(parser);
-        parser.close();
-        generator.close();
-        String actual = stringWriter.toString();
-        assertEquals(input, actual);
+        try (JsonParser parser = JSON_F.createParser(input)) {
+            parser.nextToken();
+            try (JsonGenerator generator = JSON_F.createGenerator(stringWriter)) {
+                generator.copyCurrentStructure(parser);
+            }
+        }
+        assertEquals(input, stringWriter.toString());
     }
-
 }

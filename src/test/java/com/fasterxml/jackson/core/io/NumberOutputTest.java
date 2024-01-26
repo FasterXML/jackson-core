ec03a5fbf9cbb49ccb267f53afdc2970edb70e0c
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-25 17:14:55 -0800

    Speed up manual test by avoiding String concatenation for non-failing case

--- src/test/java/com/fasterxml/jackson/core/io/NumberOutputTest.java
+++ src/test/java/com/fasterxml/jackson/core/io/NumberOutputTest.java
@@ -3,17 +3,20 @@ package com.fasterxml.jackson.core.io;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class NumberOutputTest {
     @Test
+    // Comment out for manual testing:
     @Ignore
     public void testDivBy1000() {
-        for (long i = 0; i <= Integer.MAX_VALUE; i++) {
+        for (int i = 0; i <= Integer.MAX_VALUE; i++) {
             int number = (int) i;
             int expected = number / 1000;
             int actual = NumberOutput.divBy1000(number);
-            assertEquals("input=" + number, expected, actual);
+            if (expected != actual) { // only construct String if fail
+                fail("With "+number+" should get "+expected+", got: "+actual);
+            }
         }
     }
 }

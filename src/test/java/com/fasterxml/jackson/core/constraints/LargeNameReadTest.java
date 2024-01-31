224ae67e90526f6d819ae68081b552570238d86f
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-30 20:16:51 -0800

    Fixes #1207: apply "maxNameLength" change to CharsToNameCaonicalizer

--- src/test/java/com/fasterxml/jackson/core/constraints/LargeNameReadTest.java
+++ src/test/java/com/fasterxml/jackson/core/constraints/LargeNameReadTest.java
@@ -16,6 +16,12 @@ public class LargeNameReadTest extends BaseTest
             .streamReadConstraints(StreamReadConstraints.builder().maxNameLength(100).build())
             .build();
 
+    private final JsonFactory JSON_F_NAME_100_B = new JsonFactory();
+    {
+        JSON_F_NAME_100_B.setStreamReadConstraints(StreamReadConstraints.builder()
+                .maxNameLength(100).build());
+    }
+
     // Test name that is below default max name
     public void testLargeNameBytes() throws Exception {
         final String doc = generateJSON(StreamReadConstraints.defaults().getMaxNameLength() - 100);
@@ -31,10 +37,15 @@ public class LargeNameReadTest extends BaseTest
         }
     }
 
-    public void testLargeNameWithSmallLimitBytes() throws Exception
+    public void testLargeNameWithSmallLimitBytes() throws Exception {
+        _testLargeNameWithSmallLimitBytes(JSON_F_NAME_100);
+        _testLargeNameWithSmallLimitBytes(JSON_F_NAME_100_B);
+    }
+
+    private void _testLargeNameWithSmallLimitBytes(JsonFactory jf) throws Exception
     {
         final String doc = generateJSON(1000);
-        try (JsonParser p = createParserUsingStream(JSON_F_NAME_100, doc, "UTF-8")) {
+        try (JsonParser p = createParserUsingStream(jf, doc, "UTF-8")) {
             consumeTokens(p);
             fail("expected StreamConstraintsException");
         } catch (StreamConstraintsException e) {
@@ -42,10 +53,15 @@ public class LargeNameReadTest extends BaseTest
         }
     }
 
-    public void testLargeNameWithSmallLimitChars() throws Exception
+    public void testLargeNameWithSmallLimitChars() throws Exception {
+        _testLargeNameWithSmallLimitChars(JSON_F_NAME_100);
+        _testLargeNameWithSmallLimitChars(JSON_F_NAME_100_B);
+    }
+
+    private void _testLargeNameWithSmallLimitChars(JsonFactory jf) throws Exception
     {
         final String doc = generateJSON(1000);
-        try (JsonParser p = createParserUsingReader(JSON_F_NAME_100, doc)) {
+        try (JsonParser p = createParserUsingReader(jf, doc)) {
             consumeTokens(p);
             fail("expected StreamConstraintsException");
         } catch (StreamConstraintsException e) {

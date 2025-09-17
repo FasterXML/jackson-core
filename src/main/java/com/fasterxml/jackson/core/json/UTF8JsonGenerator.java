8bdc88fcdfdb57b47f57e2c774015b4cd38bf5e3
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-09-17 13:41:00 -0700

    Refactoring

--- src/main/java/com/fasterxml/jackson/core/json/UTF8JsonGenerator.java
+++ src/main/java/com/fasterxml/jackson/core/json/UTF8JsonGenerator.java
@@ -1344,7 +1344,7 @@ public class UTF8JsonGenerator
 
         while (left > 0) {
             int len = Math.min(_outputMaxContiguous, left);
-            // avoids splitting surrogates between two segments.
+            // [core#1473]: avoid splitting surrogates between two segments.
             if (_isStartOfSurrogatePair(text.charAt(len-1))) {
                 len--;
             }

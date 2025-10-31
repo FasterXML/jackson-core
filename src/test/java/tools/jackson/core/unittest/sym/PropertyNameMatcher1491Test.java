6a75ea2b956ffe00d1e50aec123f8afbdabba584
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-10-31 10:59:15 -0700

    Moar testing

--- src/test/java/tools/jackson/core/unittest/sym/PropertyNameMatcher1491Test.java
+++ src/test/java/tools/jackson/core/unittest/sym/PropertyNameMatcher1491Test.java
@@ -1,15 +1,18 @@
 package tools.jackson.core.unittest.sym;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import tools.jackson.core.*;
+import tools.jackson.core.io.SerializedString;
 import tools.jackson.core.json.JsonFactory;
 import tools.jackson.core.sym.PropertyNameMatcher;
 import tools.jackson.core.util.Named;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // [core#1491]: PropertyNameMatcher collision
 public class PropertyNameMatcher1491Test
@@ -29,14 +32,19 @@ public class PropertyNameMatcher1491Test
 """.formatted(KEY_1, KEY_2);
 
     @Test
-    void test1491ViaRegularParser() throws Exception {
+    void test1491ViaRegularParserBytes() throws Exception {
         _testViaRegularParser(MODE_INPUT_STREAM);
         _testViaRegularParser(MODE_INPUT_STREAM_THROTTLED);
+    }
+
+    @Test
+    void test1491ViaRegularParserChars() throws Exception {
         _testViaRegularParser(MODE_READER);
     }
 
     private void _testViaRegularParser(int mode) throws Exception
     {
+        // First, regular reads
         try (JsonParser p = createParser(mode, DOC_1491)) {
             assertToken(JsonToken.START_OBJECT, p.nextToken());
             assertToken(JsonToken.PROPERTY_NAME, p.nextToken());
@@ -49,13 +57,50 @@ public class PropertyNameMatcher1491Test
             assertEquals("v4", p.getString());
             assertToken(JsonToken.END_OBJECT, p.nextToken());
         }
+
+        // Then, nextName() variants
+        try (JsonParser p = createParser(mode, DOC_1491)) {
+            assertToken(JsonToken.START_OBJECT, p.nextToken());
+            assertTrue(p.nextName(new SerializedString(KEY_1)));
+            assertToken(JsonToken.PROPERTY_NAME, p.currentToken());
+            assertEquals(KEY_1, p.currentName());
+            assertToken(JsonToken.VALUE_STRING, p.nextToken());
+            assertEquals("v3", p.getString());
+            assertTrue(p.nextName(new SerializedString(KEY_2)));
+            assertToken(JsonToken.PROPERTY_NAME, p.currentToken());
+            assertEquals(KEY_2, p.currentName());
+            assertToken(JsonToken.VALUE_STRING, p.nextToken());
+            assertEquals("v4", p.getString());
+            assertToken(JsonToken.END_OBJECT, p.nextToken());
+        }
+
+        try (JsonParser p = createParser(mode, DOC_1491)) {
+            assertToken(JsonToken.START_OBJECT, p.nextToken());
+            // try wrong match
+            assertFalse(p.nextName(new SerializedString(KEY_2)));
+            assertToken(JsonToken.PROPERTY_NAME, p.currentToken());
+            assertEquals(KEY_1, p.currentName());
+            assertToken(JsonToken.VALUE_STRING, p.nextToken());
+            assertEquals("v3", p.getString());
+            assertFalse(p.nextName(new SerializedString(KEY_1)));
+            assertToken(JsonToken.PROPERTY_NAME, p.currentToken());
+            assertEquals(KEY_2, p.currentName());
+            assertToken(JsonToken.VALUE_STRING, p.nextToken());
+            assertEquals("v4", p.getString());
+            assertToken(JsonToken.END_OBJECT, p.nextToken());
+        }
     }
 
     @Test
-    void test1491ViaMatcher() throws Exception {
+    void test1491ViaMatcherBytes() throws Exception {
         JsonFactory f = newStreamFactory();
         _testViaMatcher(f, MODE_INPUT_STREAM);
         _testViaMatcher(f, MODE_INPUT_STREAM_THROTTLED);
+    }
+
+    @Test
+    void test1491ViaMatcherChar() throws Exception {
+        JsonFactory f = newStreamFactory();
         _testViaMatcher(f, MODE_READER);
     }
 

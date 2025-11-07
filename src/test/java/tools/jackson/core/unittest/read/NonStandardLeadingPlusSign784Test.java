0d90fd86c1bbc9823dfaba969d8e305939105731
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-11-06 19:29:53 -0800

    ...

--- src/test/java/tools/jackson/core/unittest/read/NonStandardLeadingPlusSign784Test.java
+++ src/test/java/tools/jackson/core/unittest/read/NonStandardLeadingPlusSign784Test.java
@@ -24,39 +24,33 @@ class NonStandardLeadingPlusSign784Test extends JacksonCoreTestBase
     void testLeadingPlusSignConsistency() throws Exception {
         // Test various number formats with leading plus sign
         // [core#784]: All should consistently INCLUDE the '+' sign in getText()/getString()
-        _testNumber(" +125 ", "+125", JsonToken.VALUE_NUMBER_INT);
-        _testNumber(" +0.125 ", "+0.125", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +1.25e2 ", "+1.25e2", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +125.0 ", "+125.0", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +0 ", "+0", JsonToken.VALUE_NUMBER_INT);
-        _testNumber(" +1 ", "+1", JsonToken.VALUE_NUMBER_INT);
+        _testNumber("+125", JsonToken.VALUE_NUMBER_INT);
+        _testNumber("+0.125", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+1.25e2", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+125.0", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+0", JsonToken.VALUE_NUMBER_INT);
+        _testNumber("+1", JsonToken.VALUE_NUMBER_INT);
 
         // Special case: numbers starting with decimal point (issue #784)
-        _testNumber(" +.125 ", "+.125", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +.5 ", "+.5", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +.0 ", "+.0", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+.125", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+.5", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+.0", JsonToken.VALUE_NUMBER_FLOAT);
 
         // With exponents
-        _testNumber(" +1e2 ", "+1e2", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +1e+2 ", "+1e+2", JsonToken.VALUE_NUMBER_FLOAT);
-        _testNumber(" +1e-2 ", "+1e-2", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+1e2", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+1e+2", JsonToken.VALUE_NUMBER_FLOAT);
+        _testNumber("+1e-2", JsonToken.VALUE_NUMBER_FLOAT);
     }
 
-    private void _testNumber(String input, String expectedText, JsonToken expectedToken) throws Exception {
+    private void _testNumber(String numberString, JsonToken expectedToken) throws Exception {
+        String input = " " + numberString + " ";
         for (int mode : ALL_MODES) {
             try (JsonParser p = createParser(JSON_F, mode, input)) {
-                assertEquals(expectedToken, p.nextToken(),
-                    "Wrong token for input: " + input + " in mode " + mode);
-                String text = p.getText();
-                assertEquals(expectedText, text,
-                    "getText() returned wrong value for input: " + input + " in mode " + mode +
+                assertToken(expectedToken, p.nextToken());
+                String text = p.getString();
+                assertEquals(numberString, text,
+                    "getText() returned wrong value for number: " + numberString + " in mode " + mode +
                     " - got: '" + text + "'");
-
-                // Also verify getString() is consistent
-                String str = p.getString();
-                assertEquals(expectedText, str,
-                    "getString() returned wrong value for input: " + input + " in mode " + mode +
-                    " - got: '" + str + "'");
             }
         }
     }

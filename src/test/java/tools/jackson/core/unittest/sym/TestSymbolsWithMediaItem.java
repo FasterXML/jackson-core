9973de0746a7cb0b274c451e4fea959afe19ac67
PJ Fanning <pjfanning@users.noreply.github.com>
2026-05-19 21:07:11 +0100

    Update TestSymbolsWithMediaItem.java

--- src/test/java/tools/jackson/core/unittest/sym/TestSymbolsWithMediaItem.java
+++ src/test/java/tools/jackson/core/unittest/sym/TestSymbolsWithMediaItem.java
@@ -12,12 +12,42 @@ import tools.jackson.core.sym.ByteQuadsCanonicalizer;
 import tools.jackson.core.sym.CharsToNameCanonicalizer;
 import tools.jackson.core.unittest.*;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestSymbolsWithMediaItem extends JacksonCoreTestBase
 {
     private final String JSON = """
-            {"media" : {\n      "uri" : "http://foo.com",      "title" : "Test title 1",      "width" : 640, "height" : 480,      "format" : "video/mpeg4",      "duration" : 18000000,      "size" : 58982400,      "bitrate" : 262144,      "persons" : [ ],      "player" : "native",      "copyright" : "None"   },\n   "images" : [ {\n      "uri" : "http://bar.com",\n      "title" : "Test title 1",\n      "width" : 1024,"height" : 768,\n      "size" : "LARGE"\n    }, {\n      "uri" : "http://foobar.org",\n      "title" : "Javaone Keynote",\n      "width" : 320, "height" : 240,\n      "size" : "SMALL"\n    } ]\n}\n""";
+            {
+              "media" : {
+                "uri" : "http://foo.com",
+                "title" : "Test title 1",
+                "width" : 640,
+                "height" : 480,
+                "format" : "video/mpeg4",
+                "duration" : 18000000,
+                "size" : 58982400,
+                "bitrate" : 262144,
+                "persons" : [ ],
+                "player" : "native",
+                "copyright" : "None"
+              },
+              "images" : [
+                {
+                  "uri" : "http://bar.com",
+                  "title" : "Test title 1",
+                  "width" : 1024,
+                  "height" : 768,
+                  "size" : "LARGE"
+                },
+                {
+                  "uri" : "http://foobar.org",
+                  "title" : "Javaone Keynote",
+                  "width" : 320,
+                  "height" : 240,
+                  "size" : "SMALL"
+                } ]
+              }""";
 
     @Test
     void smallSymbolSetWithBytes() throws IOException
@@ -27,7 +57,7 @@ class TestSymbolsWithMediaItem extends JacksonCoreTestBase
         ByteQuadsCanonicalizer symbolsRoot = TestByteQuadsCanonicalizer.createRoot(SEED);
         ByteQuadsCanonicalizer symbols = symbolsRoot.makeChild(JsonFactory.Feature.collectDefaults());
         JsonFactory f = new JsonFactory();
-        JsonParser p = f.createParser(ObjectReadContext.empty(), JSON.getBytes("UTF-8"));
+        JsonParser p = f.createParser(ObjectReadContext.empty(), JSON.getBytes(UTF_8));
 
         JsonToken t;
         while ((t = p.nextToken()) != null) {
@@ -35,7 +65,7 @@ class TestSymbolsWithMediaItem extends JacksonCoreTestBase
                 continue;
             }
             String name = p.currentName();
-            int[] quads = calcQuads(name.getBytes("UTF-8"));
+            int[] quads = calcQuads(name.getBytes(UTF_8));
 
             if (symbols.findName(quads, quads.length) != null) {
                 continue;
